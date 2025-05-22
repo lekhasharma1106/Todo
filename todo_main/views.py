a9cab8b0b118f1585946d36b2e696eb0ee383583
Lekha Sharma <R1556@reventengineering.com>
2025-05-22 16:36:59 +0530

    todo app final

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from todo.models import Task
 def home(request):
-    return render(request,"home.html")
+    tasks=Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_task=Task.objects.filter(is_completed=True)
+    incompleted_task=Task.objects.filter(is_completed=False)
+    print(completed_task)
+    context={
+        'tasks':tasks,
+        'completed_task': completed_task,
+        'incompleted_task': incompleted_task,
+    }
+    return render(request,"home.html",context)
